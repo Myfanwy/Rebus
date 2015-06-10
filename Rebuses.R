e050f97a011934253d496be63e6b6c1ecf74b03f
Myfanwy Johnston <mrowlan1@gmail.com>
2015-06-10 12:06:28 -0700

    Update Rebuses.R

--- Rebuses.R
+++ Rebuses.R
@@ -6,16 +6,19 @@
 
 # current limitations: have to source this before you rebus() will work.
 
-## At the 11th Hour
 
-# build data
+library(ggplot2)
 Hour <- seq(1:12)
 y <- sample(0:100, 12, rep=TRUE)
 df <- as.data.frame(cbind(Hour, y))
 
 ## build plot
-bar1 <- ggplot(df, aes(Hour,y)) + 
+
+Hour <- seq(1:12)
+y <- sample(0:100, 12, rep=TRUE)
+df <- as.data.frame(cbind(Hour, y))
+ggplot(df, aes(Hour,y)) + 
   geom_bar(stat = 'identity', fill = "steelblue", colour = "black") + 
   scale_x_continuous(breaks = seq(1,12, by=1)) + 
-  geom_text(data = NULL, x = 11, y = 50, label = "@", color = "white", size = 16) + 
+  geom_text(data = NULL, x = 11, y = 40, label = "@", color = "yellow", size = 10) + 
   ylab(" ")
